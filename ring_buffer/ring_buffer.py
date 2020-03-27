a4ebb961347104844005e323bb8f5524ddfa2ed6
DAVID HANG <davidnhang@gmail.com>
2020-03-27 11:53:40 -0700

    ring_buffer update
    didn't understand because of self.current

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,4 @@
 from doubly_linked_list import DoublyLinkedList
-
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -8,25 +6,39 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        while(current is not None):
+            self.storage.add_to_head(item)
+        if self.current is None:
+            self.current = 0
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
         # TODO: Your code here
+        while(self.storage is not None):
+            list_buffer_contents.append(self.storage.remove_from_tail())
+            print(list_buffer_contents, 'asd')
+            return list_buffer_contents
 
-        return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
-
-
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
     def append(self, item):
-        pass
+        if self.current < self.capacity:
+            self.storage[self.current] = item
+            self.current += 1
+
+        if self.current == self.capacity:
+            self.current = 0
 
     def get(self):
-        pass
+        temp = []
+        for i in self.storage:
+            if i is not None:
+                temp.append(i)
+        return temp
